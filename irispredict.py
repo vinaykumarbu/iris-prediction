295139a99456f1cc485b63f71889ac0633aa0580
vinaykumarbu <43857677+vinaykumarbu@users.noreply.github.com>
2018-10-06 11:51:09 +0530

    Add files via upload

---
+++ irispredict.py
@@ -0,0 +1,14 @@
+from sklearn import svm
+from sklearn import datasets
+
+
+clf = svm.SVC()
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+clf.fit(X, y)
+import pickle
+s = pickle.dumps(clf)
+clf2 = pickle.loads(s)
+clf2.predict(X[0:1])
+y[0]
+print(clf2.predict(X[0:10]))
